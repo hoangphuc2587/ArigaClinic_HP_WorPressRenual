e4873cfea1ff44d237d59901dfc1ef19c9bd5ccb
Truong Long <truonglong12a1@gmail.com>
2020-10-02 23:10:57 +0700

    done page-support

--- wp-content/themes/twentytwenty/templates/template-contact.php
+++ wp-content/themes/twentytwenty/templates/template-contact.php
@@ -21,7 +21,51 @@ get_header();
  </div>
 </div>
 
+<div class="container amore-inner-container" style="margin-bottom:0px">
+ <div class="row">
+   <div class="col-sm-120 col-xs-120 no-padding single-page">
+      <h4><span style="margin-left: 30px;">初めてのホーチミンありが歯科ご来院の流れ</span></h4>
+      <h2><b>【日本語専用】：０９０ - ４１８ - ６４８０ </b></h2><br />
+      <h2><b>【診療時間】</b></h2>
+         <div style="margin-left: 210px;">
+            <table style="width:70%;"> 
+               <tr>
+                  <th style="background-color: #D6C77E;"></th>
+                  <th style="background-color: #D6C77E;">午前</th>
+                  <th style="background-color: #D6C77E;">午後</th>
+               </tr>
+               <tr style="text-align: center;">
+                  <td>月曜~金曜</td>
+                  <td>09:00 ~ 12:00</td>
+                  <td>14:00 ~ 19:00</td>
+               </tr>
+               <tr style="text-align: center;">
+                  <td>土曜</td>
+                  <td>09:00 ~ 12:00</td>
+                  <td>13:00 ~ 17:00</td>
+               </tr>
+            </table>
+         </div>
+         <h2>【休診日】</h2>
+         <h2><span style="margin-left: 60px;">木曜(午前) / 日曜日</span></h2>
 
+      <h4><span style="margin-left: 30px;">LINEでの予約</span></h4>
+
+         <div style="width: 20%; float:left; padding-left:40px;">
+            <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/10/PIp-mJw6A.png" alt="qr-code">
+         </div>
+   
+         <div style="width: 80%; float:right; padding-left:70px;">
+             <p>友達追加後、以下情報をお送りください <br />【ご予約日】<br />【お名前】<br />【 治療内容】</p>
+         </div>
+
+      <h4><span style="margin-left: 30px;">住所</span></h4>
+      <h2 style="text-align: center;">住所：140 Ký Con, Phường Nguyễn Thái Bình, Quận 1, Thành phố Hồ Chí Minh</h2>
+      <iframe style="margin-left: 130px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.564296312543!2d106.69560971480071!3d10.768023792327188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f3fda4c70ff%3A0x41f9fe114ec78365!2zMTQwIEvDvSBDb24sIFBoxrDhu51uZyBOZ3V54buFbiBUaMOhaSBCw6xuaCwgUXXhuq1uIDEsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaA!5e0!3m2!1sja!2s!4v1601649761036!5m2!1sja!2s" width="800" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+   </div> 
+   
+ </div>
+</div>
 </div>
 
 <?php get_footer(); ?>
