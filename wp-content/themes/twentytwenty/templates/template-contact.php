b0bb8be6499ac2187c56a883e7623a9a888225d2
Truong Long <truonglong12a1@gmail.com>
2020-10-02 18:13:19 +0700

    update code

--- wp-content/themes/twentytwenty/templates/template-contact.php
+++ wp-content/themes/twentytwenty/templates/template-contact.php
@@ -10,8 +10,18 @@
 
 get_header();
 ?>
+<div class="main-content">
+<div class="amore-divider romaji" data-parallax="scroll" data-image-src="<?php echo home_url(); ?>/wp-content/uploads/2020/10/surface-945444_1920.jpg">
+ <div class="container">
+    <div class="row">
+       <div class="col-xs-120 no-padding">
+          <h1 class="top-headline" style="margin-top: 50px; margin-bottom: -20px;">お問い合わせ</h1>
+       </div>
+    </div>
+ </div>
+</div>
 
 
-
+</div>
 
 <?php get_footer(); ?>
