8992255f494263cbc5ceaea40b7669d4b7955f52
Truong Long <truonglong12a1@gmail.com>
2020-10-05 13:53:57 +0700

    update page support & first-visit

--- wp-content/themes/twentytwenty/templates/template-first-visit.php
+++ wp-content/themes/twentytwenty/templates/template-first-visit.php
@@ -36,7 +36,7 @@ get_header();
     <div class="col-sm-120 col-xs-120 no-padding single-page">
        <header class="breadcrumb mb20"> <a href="<?php echo home_url(); ?>"><span><img src="<?php echo home_url(); ?>/wp-content/themes/twentytwenty/assets/images/home.png" class="breadcrumb-home"></span></a> <span class="fa fa-chevron-right text-muted" style="font-size:10px">&nbsp;</span> <span>初めてのありが歯科</span></header>
        <div class="row article" style="margin:auto 0;">
-          <h3 class="ppt">Welcome ホーチミンのありが歯科へ</h3>
+          <h3 class="ppt">Welcome ホーチミンのありが歯科へ</h3><br />
           <p>ホーチミンのありが歯科では患者様の声に耳を傾け、ご希望やご要望を知る。<br /> 何気ない言動から患者様の真意を知る努力をする。<br /> そして歯科医師としての専門家から意見を述べさせていただき、治療方針を立てる。<br />このような医療人として当たり前の行為をきちんとこなす。<br />これが『<b>ありが歯科スタイル</b>』<br />せっかく治療をするならきちんと綺麗に治して欲しい。
           <br />ホーチミンのありが歯科では、ご要望にお応えし、患者様に安心して歯科治療をホーチミンで受けていただけるように私たちは努力します。</p>
           
