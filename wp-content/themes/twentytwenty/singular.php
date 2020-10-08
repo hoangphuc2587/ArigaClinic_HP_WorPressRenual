b04df85ec499009ecbef22879f03d41ad75a0acf
Phuc Nguyen <nhphuc@starboardasia.com>
2020-10-08 14:33:12 +0700

    update news detail map

--- wp-content/themes/twentytwenty/singular.php
+++ wp-content/themes/twentytwenty/singular.php
@@ -178,3 +178,62 @@ get_header();
  </div>
 </div>
 <?php get_footer(); ?>
+<script type="text/javascript">
+  if($('#map999921').length > 0){
+   var loaded_map999921 = false;
+   var timer_map999921;
+   
+   timer_map999921 = setInterval(function(){
+     if(google && !loaded_map999921){
+       clearInterval(timer_map999921);
+       loaded_map999921 = true;
+   
+       var disabled = false;
+       var draggable = true;
+       var geocoder = new google.maps.Geocoder();
+   
+       geocoder.geocode({address: '140 ky con Q1 HCMC'}, function(results, status) {
+         if(status == google.maps.GeocoderStatus.OK){
+           var mapOptions = {
+             draggable: draggable == true ? true : false,
+             center: results[0].geometry.location,
+             zoom: 15,
+             mapTypeId: google.maps.MapTypeId.ROADMAP,
+             scrollwheel: false,
+             disableDefaultUI: disabled,
+             disableDoubleClickZoom: disabled,
+             styles: [{"featureType":"landscape","stylers":[{"hue":"#F1FF00"},{"saturation":-27.4},{"lightness":9.4},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#0099FF"},{"saturation":-20},{"lightness":36.4},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#00FF4F"},{"saturation":0},{"lightness":0},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FFB300"},{"saturation":-38},{"lightness":11.2},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00B6FF"},{"saturation":4.2},{"lightness":-63.4},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#9FFF00"},{"saturation":0},{"lightness":0},{"gamma":1}]}]
+           };
+   
+           var map = new google.maps.Map(document.getElementById('map999921'), mapOptions);
+           var marker = new google.maps.Marker({
+               map: map,
+               position: results[0].geometry.location
+           });
+   
+           var map_changed = false;
+           if(draggable){
+             google.maps.event.addListener(map, 'dragstart', function(){
+               map_changed = true;
+             });
+           }
+   
+           var map_resize = function(){
+             google.maps.event.trigger(map, 'resize');
+             if(map_changed){
+               google.maps.event.removeListener(resize_listener);
+             }else{
+               map.setCenter(results[0].geometry.location);
+             }
+           };
+   
+           var resize_listener = google.maps.event.addDomListener(window, 'resize', map_resize);
+   
+           setTimeout(map_resize, 1000);
+           google.maps.event.addDomListenerOnce(window, 'load', map_resize);
+         }
+       });
+     }
+   }, 500);
+ }
+</script>
