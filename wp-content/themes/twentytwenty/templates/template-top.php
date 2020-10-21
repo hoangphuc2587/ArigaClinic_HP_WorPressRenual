a6dd1bf414c7f5702c7ca446e21844bf696e7b4f
Phuc Nguyen <hoangphuc2587@gmail.com>
2020-10-21 23:23:36 +0700

    update top page

--- wp-content/themes/twentytwenty/templates/template-top.php
+++ wp-content/themes/twentytwenty/templates/template-top.php
@@ -54,12 +54,12 @@ get_header();
        </div>
     </div>
    </section>
-   <section>
+   <section style="padding-top: 20px;">
     <div id="third" class="container">
        <div class="row">
         <div class="col-xs-120 no-padding">
           <h3 class="text-center text-header">お知らせ <span>NEWS</span></h3>
-          <div class="amore-section">
+          <div class="amore-section amore-section-list">
              <ul>
                 <li><span>2020.12.04</span> <a href="#">年末年始のお休みについて。</a></li>
                 <li><span>2020.12.04</span> <a href="#">年末年始のお休みについて。</a></li>
@@ -83,17 +83,17 @@ get_header();
     <div id="fourth" class="container greeting-top">
        <div class="row">         
          <div class="col-xs-120 no-padding">
-         <h3 class="lead romaji top-headline2 mb50">GREETING</h3>
+         <h3 class="lead romaji top-headline-left col-sm-offset-10  top-headline2 mb50">GREETING</h3>
          <div class="row">
-          <div class="col-sm-60">
+          <div class="col-sm-50 col-sm-offset-10">
             <h3>日本人歯科医師がいる <br/>安心の歯科医院</h3>
             <p>ホーチミンのありが歯科では、<span style="color:#B16E6E">可能な限り歯を抜かず削らない</span>ことで歯の寿命を延ばすことに努力しています。</p>
             <p>日本人に合った歯医者さん、歯科医院での治療内容をわかりやすく丁寧にご説明し、納得していただいた上で診察・治療を行なっています。</p>
             <p>『虫歯の治療』『歯周病』『口臭』『歯並びの矯正』『かみ合わせ』『お子様の治療』歯を自然な白い色にする『ホワイトニング』『クリーニング』失った歯の『インプラント治療』まで、患者様にご安心して日本の歯医者さんの治療をしていただけます。</p>
             <p>最新の歯科用CTも完備されており、インプラントや親知らずの抜歯、歯の根の治療などで利用でき、施設面でも充実しております。</p>
           </div>
-          <div class="col-sm-60">
-              <img src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/images_1.png">
+          <div class="col-sm-50 col-sm-offset-10">
+              <img style="width: 90%" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/images_1.png">
           </div>
          </div>
         </div>
@@ -104,8 +104,8 @@ get_header();
     <div id="fifth" class="container">
        <div class="row">
         <div class="col-xs-120 no-padding text-center">
-         <h3 class="lead romaji top-headline2" style="color: #ffff">CARE</h3>
-         <h4 style="color:#fff">治療一覧</h4>
+         <h3 class="lead romaji top-headline-center top-headline2" style="color: #ffff;position: relative;padding-top: 30px"><span>CARE</span></h3>
+         <h4 style="color:#fff;margin-bottom: 100px;margin-top: 40px">治療一覧</h4>
          <div class="row" style="padding-bottom: 40px">
             <div class="col-sm-24">
                <div class="circle">
@@ -123,58 +123,58 @@ get_header();
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top: 40px;" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_3.png">
+                 <img style="margin-top: 30px;" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_3.png">
                  <h3 class="text-inline">PAIN</h3>
                </div>
                <h3 class="text-outline">痛い・しみる</h3>
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top: 55px;" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_4.png">
+                 <img style="margin-top: 45px;" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_4.png">
                  <h3 class="text-inline">WHITENING</h3>
                </div>
                <h3 class="text-outline">白くしたい</h3>
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top: 50px;" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_5.png">
+                 <img style="margin-top: 35px;" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_5.png">
                  <h3 class="text-inline">PERIODONTITIS</h3>
                </div>
                <h3 class="text-outline">歯周病治療</h3>
             </div>  
          </div>
-           <div class="row">
+            <div class="row" style="padding-bottom: 40px">
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top:55px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_6.png">
+                 <img style="margin-top:40px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_6.png">
                  <h3 class="text-inline">BREATH</h3>
                </div>
                <h3 class="text-outline">口臭対策</h3>
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top:70px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_7.png">
+                 <img style="margin-top:60px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_7.png">
                  <h3 class="text-inline">WISDOM TOOTH</h3>
                </div>
                <h3 class="text-outline">親知らずが痛い</h3>
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top:80px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_8.png">
+                 <img style="margin-top:70px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_8.png">
                  <h3 class="text-inline">ORTHODONIC</h3>
                </div>
                <h3 class="text-outline">歯並び・歯科矯正</h3>
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top:38px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_9.png">
+                 <img style="margin-top:28px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_9.png">
                  <h3 class="text-inline">SECOND OPINION</h3>
                </div>
                <h3 class="text-outline">セカンドオピニオン</h3>
             </div>
             <div class="col-sm-24">
                <div class="circle">
-                 <img style="margin-top:50px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_10.png">
+                 <img style="margin-top:40px" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/care_1_10.png">
                  <h3 class="text-inline">IMPLANT</h3>
                </div>
                <h3 class="text-outline">インプラント</h3>
@@ -185,18 +185,159 @@ get_header();
     </div>
    </section>
    <section>
-    <div id="sixth" class="has-background" style="background-image:url(https://arigadc.com/wp-content/uploads/tcd-w/7IM7G_2465.jpg)">
-       <div class="container" style="overflow-x:hidden">
-        <div class="row amore-section">
-         <div class="col-xs-120 no-padding-mobile">
-          <div class="row map-wrap-dark">
-             <div class="col-sm-60 no-left-padding no-padding-mobile" style="line-height:30px;"> ホーチミンのありが歯科では、衛生面でも清潔を保ち、可能な限り痛くない・削らない虫歯治療、中等度・重度のかたも対応できる歯周病(歯槽膿漏)治療、PMTC,スケーリングなどの予防歯科、こども歯科(フッ素、シーラント塗布)はもとより、入れ歯治療(目立たない・痛くない・はずれない)、歯並び・噛み合わせを正す矯正歯科治療(大人(成人)・子供(小児)）も対応。審美歯科治療(ジルコニア、オールセラミック)、ホームホワイトニング、インプラント（骨造成、骨再生療法も行っておりますので、骨が少ない方でも対応可能です）を行っております。また、セカンドオピニオンにも対応しております。歯科用ＣＴ完備,浄水器完備<br /> <br /> 　　　　　ありが歯科<br /> 　　　　良い歯　虫歯ゼロ　<br /> <font size="5">090-418-6480 日本語専用</font><br /> LINE@でもご予約可能です。<br /> <img src="https://arigadc.com/wp-content/uploads/2019/04/PIp-mJw6A.png" alt="サンプル"><br /> モッ　ボーン　ムイ　キーコン　クウォン　モッ<br /> <font size="5">140  Ký  Con, Quận 1 THÀNH PHỐ HỒ CHÍ MINH<br /> 71010 </font><br /> <img class="alignnone wp-image-245 size-full" src="https://arigadc.com/wp-content/uploads/2018/08/time3.jpg" alt="" width="550" height="196" /><br /> 木曜午前休診　日祝休診<br /> お電話にてご予約よろしくお願いします。</div>
-             <div class="visible-xs mb40"></div>
-             <div class="col-sm-60 no-right-padding no-padding-mobile">
-              <!-- <div id="map-canvas" style="width:100%; height:480px; border:4px solid white; border-radius:2px;"></div> -->
-             </div>
+    <div id="sixth" class="container support-top" style="padding-top: 40px;">
+       <div class="row">         
+         <div class="col-xs-120 no-padding">
+         <h3 class="lead romaji text-center top-headline-center top-headline2" style="position: relative;padding-top: 30px"><span>SUPPORT</span></h3>
+         <h4 class="text-center" style="margin-bottom: 100px;margin-top: 40px">治療一覧</h4>
+         <div class="row">
+          <div class="col-sm-50 col-sm-offset-10">
+              <img style="width: 90%" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/images_2.png">
+          </div>
+          <div class="col-sm-50" style="margin-right: 8.33333333%;">
+            <h3 class="title-header-first" style="padding: 30px 0px;">日本人歯科医師がいる</h3>
+            <h4 style="padding: 0px 0px 30px 0px;text-transform: uppercase;font-weight: 700">First</h3>
+        
+            <p style="line-height: 27px">『虫歯の治療』『歯周病』『口臭』『歯並びの矯正』『かみ合わせ』『お子様の治療』歯を自然な白い色にする『ホワイトニング』『クリーニング』失った歯の『インプラント治療』まで、患者様にご安心して日本の歯医者さんの治療をしていただけます。</p>
+
+            <div class="row" style="padding-top: 100px">
+              <div class="col-sm-120 text-center">
+                   <a style="border: 2px solid #ccc;
+    padding: 15px 90px;
+    border-radius: 55px;color: #8B8679;text-decoration: none;" href="<?php echo home_url(); ?>/first-visit/">患者様</a>
+              </div> 
+            </div> 
           </div>
          </div>
+
+         <div class="row" style="padding-top: 100px">
+         
+          <div class="col-sm-50 col-sm-offset-10" >
+            <h3 class="title-header-staff" style="padding: 30px 0px;">日本人歯科医師がいる</h3>
+            <h4 style="padding: 0px 0px 30px 0px;text-transform: uppercase;font-weight: 700">STAFF</h3>
+        
+            <p style="line-height: 27px">『虫歯の治療』『歯周病』『口臭』『歯並びの矯正』『かみ合わせ』『お子様の治療』歯を自然な白い色にする『ホワイトニング』『クリーニング』失った歯の『インプラント治療』まで、患者様にご安心して日本の歯医者さんの治療をしていただけます。</p>
+
+            <div class="row" style="padding-top: 100px">
+              <div class="col-sm-120 text-center">
+                   <a style="border: 2px solid #ccc;
+    padding: 15px 90px;
+    border-radius: 55px;color: #8B8679;text-decoration: none;" href="<?php echo home_url(); ?>/first-visit/">患者様</a>
+              </div> 
+            </div> 
+          </div>
+
+           <div class="col-sm-50" style="margin-right: 8.33333333%;">
+              <img style="width: 90%" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/images_3.png">
+          </div>
+         </div>
+
+
+         <div class="row" style="padding-top: 100px">
+          <div class="col-sm-50 col-sm-offset-10">
+              <img style="width: 90%" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/images_4.png">
+          </div>
+          <div class="col-sm-50" style="margin-right: 8.33333333%;">
+            <h3 class="title-header-first" style="padding: 30px 0px;">日本人歯科医師がいる</h3>
+            <h4 style="padding: 0px 0px 30px 0px;text-transform: uppercase;font-weight: 700">Promise</h3>
+        
+            <p style="line-height: 27px">『虫歯の治療』『歯周病』『口臭』『歯並びの矯正』『かみ合わせ』『お子様の治療』歯を自然な白い色にする『ホワイトニング』『クリーニング』失った歯の『インプラント治療』まで、患者様にご安心して日本の歯医者さんの治療をしていただけます。</p>
+
+            <div class="row" style="padding-top: 100px">
+              <div class="col-sm-120 text-center">
+                   <a style="border: 2px solid #ccc;
+    padding: 15px 90px;
+    border-radius: 55px;color: #8B8679;text-decoration: none;" href="<?php echo home_url(); ?>/promise/">患者様</a>
+              </div> 
+            </div> 
+          </div>
+         </div>
+
+        </div>
+       </div>
+    </div>
+   </section>
+   <section style="padding: 120px 0px">
+    <div class="amore-divider romaji" data-parallax="scroll" data-speed="0.6" data-image-src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/images_5.png">
+       <div class="container">
+        <div class="row">
+         <div class="col-xs-120 no-padding">
+          <h2 class="invisibletexteffect animate offsetted top-headline fourth_headline"></h2>
+         </div>
+        </div>
+       </div>
+    </div>
+  </section>
+  <section style="background: #E8E4DC;">
+    <div id="seventh" class="container category-top">
+       <div class="row">         
+         <div class="col-xs-120 no-padding">
+         <h3 class="lead romaji text-center top-headline-category top-headline2" style="position: relative;padding-top: 30px;text-transform: uppercase;font-weight: 700"><span>Category</span></h3>
+         <h4 class="text-center" style="margin-bottom: 100px;margin-top: 40px">治療一覧</h4>
+
+         <div class="row">
+          <div class="col-sm-100 col-sm-offset-10"  style="margin-right: 8.33333333%;">
+              <div class="row">
+                <div class="col-sm-40 no-padding">
+                  <a href="#">
+                   <img class="category-img" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/category-1.png" />
+                   <div class="caption-text">
+                                    
+                   </div> 
+                    <div class="text-center">
+                      <h3>治療一覧</h3>
+                      <h4>CLEANING</h4>
+                     </div> 
+                   </a>
+                </div>
+
+                <div class="col-sm-40 no-padding">
+                  <a href="#">
+                    <img class="category-img" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/category-2.png" />
+                    <div class="caption-text">
+                     <h3>治療一覧</h3>
+                     <h4>CLEANING</h4>
+                   </div> 
+                  </a>
+                </div>
+
+                <div class="col-sm-40 no-padding"><a href="#"><img class="category-img" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/category-3.png" />
+                  <div class="caption-text">
+                     <h3>治療一覧</h3>
+                     <h4>CLEANING</h4>
+                   </div> 
+                </a></div>
+
+              </div>  
+              <div class="row">
+                <div class="col-sm-40 no-padding"><a href="#">
+                  <img class="category-img" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/category-4.png" />
+                  <div class="caption-text">
+                     <h3>治療一覧</h3>
+                     <h4>CLEANING</h4>
+                   </div> 
+                </a>
+                </div>
+
+                <div class="col-sm-40 no-padding"><a href="#"><img class="category-img" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/category-5.png" />
+                  <div class="caption-text">
+                     <h3>治療一覧</h3>
+                     <h4>CLEANING</h4>
+                   </div>
+                </a></div>
+
+                <div class="col-sm-40 no-padding"><a href="#"><img class="category-img" src="<?php echo home_url(); ?>/wp-content/uploads/tcd-w/category-6.png" />
+                  <div class="caption-text">
+                     <h3>治療一覧</h3>
+                     <h4>CLEANING</h4>
+                   </div>
+                </a></div>
+
+              </div>  
+
+          </div>       
+         </div>
+
         </div>
        </div>
     </div>
